ec0adc6ba8a0278466b9a81eafa8bd4f25c3069f
Arthur Cerqueira <92320774+arthurcerqueirm@users.noreply.github.com>
2022-10-23 15:09:16 -0300

    dwd

--- java.js
+++ java.js
@@ -1,10 +1,17 @@
 const menu = document.querySelector('.menu')
 const btnMenu = document.querySelector('.btn-menu')
 const header = document.querySelector('#header')
+var clicar = document.querySelector('.nao')
 
 function OpenMenu(){
+    console.log(clicar)
     menu.classList.toggle("active")
     window.setTimeout( header.classList.toggle("active"), 600)
 }
+
+function FecharMenu(){
+    menu.classList.toggle("active")
+}
+
 
 btnMenu.addEventListener('click' , OpenMenu)
